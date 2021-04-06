6126f7b740cb232d12c6c1d3522e687ca0af9a41
timnorov <timur.norov@gmail.com>
2021-04-06 19:28:54 +0300

    Submitted leson03

--- script/script.js
+++ script/script.js
@@ -1,14 +1,38 @@
-let num = 266219;
-let mul = 1;
-let digit;
+let lang = null;
+let langArray = [];
+let weekEn = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday','Saturday','Sunday'];
+let weekRu = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница','Суббота','Воскресенье'];
+let namePerson = null;
+let result = null;
 
-while (num > 0) {
-  digit = num % 10;
-  mul = mul * digit;
-  num = Math.floor( num / 10);
+lang = 'en';
+
+if (lang === 'en') {
+  console.log(weekEn);
+} else if (lang === 'ru'){
+  console.log(weekRu);
+} else {
+  console.log('Я пока не знаю этот язык');
 }
 
-console.log(String(mul **= 3).slice(0,2));
+switch (lang) {
+  case 'en':
+    console.log(weekEn);
+    break;
+  case 'ru':
+    console.log(weekRu);
+    break;
+  default:
+    console.log('Я пока не знаю этот язык');
+}
 
+langArray.ru = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница','Суббота','Воскресенье'];
+langArray.en = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday','Saturday','Sunday'];
 
+console.log(langArray[lang]);
 
+namePerson = 'Aртём';
+result = (namePerson === 'Aртём') ? 'Директор' : 
+  (namePerson === 'Максим') ? 'Преподаватель':
+  'студент';
+console.log(result);
