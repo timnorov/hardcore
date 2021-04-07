6abdc6954f1fa81eb32604942a4c3ddbae6893ae
timnorov <timur.norov@gmail.com>
2021-04-07 22:13:13 +0300

    Submitted lesson05

--- script/script.js
+++ script/script.js
@@ -1,14 +1,27 @@
-let num = 266219;
-let mul = 1;
-let digit;
+'use strict';
+//Усложненное задание №1
+let arr = ['24', '34', '65', '254', '456', '334', '54'];
+let res = 0;
 
-while (num > 0) {
-  digit = num % 10;
-  mul = mul * digit;
-  num = Math.floor( num / 10);
-}
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].charAt(0) == 2 || arr[i].charAt(0) == 4) {
+      console.log(arr[i]);
+    }
+  }
 
-console.log(String(mul **= 3).slice(0,2));
-
-
-
+//Усложненное задание №2
+  let number,
+      count,
+      array = [];
+    for (let i = 2; i < 100; i++) {
+        number = true;
+        for (count = 2; count < i; count++) {
+            if (i % count == 0) {
+            number = false;
+            }
+        }
+        if (number) {
+          array.push('Делители этого числа: 1 и '+ i);      
+    }
+  }
+console.log(array.join('\n'));
