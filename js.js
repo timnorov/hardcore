de200a5306791ab036f7d51e287b1e5aa95d5df7
timnorov <timur.norov@gmail.com>
2021-04-22 23:04:14 +0300

    Submitted lesson18-2

--- js.js
+++ js.js
@@ -1,17 +1,40 @@
-'use strict';
 
-const input = document.querySelector('.input'),
-	text = document.querySelector('.text'),
-	doneTypingInterval = 300;
-let typingTimer;
 
-input.addEventListener('keyup', () => {
-	clearTimeout(typingTimer);
-	if (input.value) {
-		typingTimer = setTimeout(doneTyping, doneTypingInterval);
+const train = document.querySelector('.train'),
+	start = document.querySelector('.start'),
+	pause = document.querySelector('.pause'),
+	reset = document.querySelector('.reset');
+let count = 0,
+	driveInterval,
+	isRunning = true;
+
+const trainDrive = function() {
+	 driveInterval = requestAnimationFrame(trainDrive);
+	count++;
+	if (count < 1100) {
+		train.style.left = count + 'px';
+	} else {
+		cancelAnimationFrame(driveInterval);
 	}
+};
+
+start.addEventListener('click', () => {
+	 if (isRunning) {
+		 driveInterval = requestAnimationFrame(trainDrive);
+		 start.style.display = 'none';
+		 pause.style.display = 'inline-block';
+	}
+	isRunning = !isRunning;
 });
 
-function doneTyping() {
-	text.textContent = input.value;
-}
+pause.addEventListener('click', () => {
+	start.style.display = 'inline-block';
+	pause.style.display = 'none';
+	isRunning = !isRunning;
+	cancelAnimationFrame(driveInterval);
+});
+
+reset.addEventListener('click', () => {
+	train.style.left = 0 + 'px';
+	count = 0;
+});
